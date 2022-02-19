27f322989dc790ff132f6466942602374e69a4e6
Dmitry Barskov <dmitry.barskov64@gmail.com>
2022-02-20 00:21:47 +0300

    Setup docker-compose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create!(
+  title: "How to setup docker for development",
+  body: "First I created `Dockerfile` with the next content:..."
+)
+
+User.create!(
+  email: "user@example.org",
+  first_name: "John",
+  last_name: "Smith"
+)
